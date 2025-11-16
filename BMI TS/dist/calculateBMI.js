39c0f3fdaf2aa65aab0073a683cad9930a98a1a9
Majka1012 <maja.andrysik@gmail.com>
2025-11-16 21:55:03 +0100

    Changing code style to classes

--- BMI TS/dist/calculateBMI.js
+++ BMI TS/dist/calculateBMI.js
@@ -1,18 +1,3 @@
-// export const calculateBMI = (height:number, weight:number):[number,string] => {
-//   const BMI = weight / (height * height);
-//   if (BMI < 18.5) {
-//     return [BMI, "Underweight"];
-//   }
-//   if (BMI < 25) {
-//     return [BMI, "Correct"];
-//   }
-//   if (BMI < 30) {
-//     return [BMI, "Overweight"];
-//   } else {
-//     return [BMI, "Obese"];
-//   }
-//   //   console.log(weight / (height * height));
-// };
 export class BMIClass {
     height;
     weight;
--- BMI TS/src/app.ts
+++ BMI TS/src/app.ts
@@ -107,3 +107,33 @@ const render = (ElCreate: string, className: string, innerHTML: string, id: stri
   listRoot.append(output);
   console.log(`Rendering ${output}`);
 };
+
+class Render {
+  output: HTMLUnknownElement;
+  constructor(
+    public ElCreate: string,
+    public className: string,
+    public innerHTML: string,
+    public id: string,
+    public root: string,
+    public color?: string
+  ) {
+    this.output = document.createElement(this.ElCreate);
+    this.output.innerHTML = this.innerHTML;
+    this.output.className = this.className;
+    this.output.id = this.id;
+    if (this.color) {
+      this.output.style.backgroundColor = this.color;
+    }
+
+    this.rendering();
+  }
+  rendering() {
+    const listRoot = document.getElementById(this.root)!;
+    if (listRoot.children.length == 2 || this.ElCreate === "p") {
+      listRoot.innerHTML = "";
+    }
+    listRoot.append(this.output);
+    console.log(`Rendering ${this.output}`);
+  }
+}
